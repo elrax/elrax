2c72db2d4c36c7dd4923e6e337db2a93a706839e
Olexandr Salikovskiy <ssalikovskiy@gmail.com>
2024-03-18 13:13:19 +0100

    fix: comments bottom sheet position, scrolling & behavior on keyboard visibility, and add input field

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -2,7 +2,9 @@ import React, { useEffect } from "react"
 import { SplashScreen, router } from "expo-router"
 import { Slot } from "expo-router"
 import NetInfo from "@react-native-community/netinfo"
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet"
 import { onlineManager } from "@tanstack/react-query"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
 import { CacheManagerProvider, LFUPolicy } from "react-native-cache-video"
 import { TRPCProvider } from "~/utils/api"
 import { useIconFont } from "~/components/Icon"
@@ -52,7 +54,11 @@ export default function RootLayout() {
 	return (
 		<CacheManagerProvider cachePolicy={lfuPolicyRef.current}>
 			<TRPCProvider>
-				<Slot />
+				<GestureHandlerRootView style={{ flex: 1 }}>
+					<BottomSheetModalProvider>
+						<Slot />
+					</BottomSheetModalProvider>
+				</GestureHandlerRootView>
 			</TRPCProvider>
 		</CacheManagerProvider>
 	)
--- apps/mobile/src/components/Comments.tsx
+++ apps/mobile/src/components/Comments.tsx
@@ -1,14 +1,24 @@
-import React, { useCallback, useRef } from "react"
+import React, { useCallback, useImperativeHandle, useRef, forwardRef } from "react"
 import { StyleSheet, View, Text, Pressable } from "react-native"
-import BottomSheet, { BottomSheetFlatList } from "@gorhom/bottom-sheet"
+import {
+	BottomSheetModal,
+	BottomSheetView,
+	BottomSheetBackdrop,
+	BottomSheetFooter,
+	BottomSheetTextInput,
+} from "@gorhom/bottom-sheet"
 import { Image } from "expo-image"
 import { Icon } from "~/components/Icon"
+import { FlatList } from "react-native-gesture-handler"
 
 interface CustomBottomSheetProps {
-	isVisible: boolean
 	onClose?: () => void
 	onOpen?: () => void
 	snapPoints?: (string | number)[]
+}
+export interface CommentsMethods {
+	presentModal: () => void
+	closeModal: () => void
 }
 
 interface CommentItem {
@@ -21,22 +31,18 @@ const images = {
 	pfp: require("../assets/pfp.png"),
 }
 
-export const Comments: React.FC<CustomBottomSheetProps> = ({
-	isVisible,
-	onClose,
-	snapPoints = ["65%", "90%"],
-}) => {
-	const bottomSheetRef = useRef<BottomSheet>(null)
+export const Comments = forwardRef<CommentsMethods, CustomBottomSheetProps>((props, ref) => {
+	const bottomSheetRef = useRef<BottomSheetModal>(null)
+	const snapPoints = ["60%", "100%"]
 
-	const handleSheetChanges = useCallback(
-		(index: number) => {
-			console.log("handleSheetChanges", index)
-			if (index === -1 && onClose) {
-				onClose()
-			}
-		},
-		[onClose],
-	)
+	useImperativeHandle(ref, () => ({
+		presentModal: () => bottomSheetRef.current?.present(),
+		closeModal: () => bottomSheetRef.current?.close(),
+	}))
+
+	const handleSheetChanges = useCallback((index: number) => {
+		console.log("handleSheetChanges", index)
+	}, [])
 	const comments: CommentItem[] = [
 		{
 			username: "Jane Doe",
@@ -69,6 +75,11 @@ export const Comments: React.FC<CustomBottomSheetProps> = ({
 			text: "World Central Kitchen's commitment to providing food during crises and their impact on affected communities is truly invaluable.",
 		},
 	]
+	const renderBackdrop = useCallback(
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		(props: any) => <BottomSheetBackdrop {...props} pressBehavior="close" />,
+		[],
+	)
 	const renderItem = useCallback(
 		({ item }: { item: CommentItem }) => (
 			<View className="flex gap-2.5 flex-row px-4 py-2.5">
@@ -93,34 +104,58 @@ export const Comments: React.FC<CustomBottomSheetProps> = ({
 		),
 		[],
 	)
+	const renderActivePostSheetFooter = useCallback(
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		(props: any) => (
+			<BottomSheetFooter style={styles.footerContainer} {...props}>
+				<View>
+					<BottomSheetTextInput
+						placeholder="Add your comment"
+						style={styles.bottomSheetInput}
+						placeholderTextColor="#9A9BA2"
+					/>
+				</View>
+			</BottomSheetFooter>
+		),
+		[],
+	)
 
 	return (
-		<BottomSheet
+		<BottomSheetModal
 			ref={bottomSheetRef}
 			snapPoints={snapPoints}
 			onChange={handleSheetChanges}
 			enablePanDownToClose={true}
 			backgroundStyle={styles.bg}
 			handleIndicatorStyle={styles.indicator}
-			index={isVisible ? 0 : -1}
+			backdropComponent={renderBackdrop}
+			footerComponent={renderActivePostSheetFooter}
+			keyboardBlurBehavior="restore"
+			keyboardBehavior="interactive"
+			topInset={150}
+			index={1}
 		>
-			<View className="flex">
-				<View className="flex justify-between items-center px-4 py-3 flex-row">
-					<Text className="text-white font-ns-bold text-base">5.3K comments</Text>
-					<Pressable className="flex flex-row gap-1 items-center">
-						<Text className="text-[#9A9BA2] font-ns-body text-base">All (default)</Text>
-						<Icon name="chevron-down" size={20} color="#9A9BA2" />
-					</Pressable>
+			<BottomSheetView>
+				<View className="flex">
+					<View className="flex justify-between items-center px-4 py-3 flex-row">
+						<Text className="text-white font-ns-bold text-base">5.3K comments</Text>
+						<Pressable className="flex flex-row gap-1 items-center">
+							<Text className="text-[#9A9BA2] font-ns-body text-base">
+								All (default)
+							</Text>
+							<Icon name="chevron-down" size={20} color="#9A9BA2" />
+						</Pressable>
+					</View>
 				</View>
-			</View>
-			<BottomSheetFlatList
-				data={comments.concat(comments)}
-				keyExtractor={(item, index) => index.toString()}
-				renderItem={renderItem}
-			/>
-		</BottomSheet>
+				<FlatList
+					data={comments.concat(comments)}
+					keyExtractor={(item, index) => index.toString()}
+					renderItem={renderItem}
+				/>
+			</BottomSheetView>
+		</BottomSheetModal>
 	)
-}
+})
 
 const styles = StyleSheet.create({
 	indicator: {
@@ -132,5 +167,19 @@ const styles = StyleSheet.create({
 	bg: {
 		backgroundColor: "#1A232C",
 		borderRadius: 24,
+	},
+	footerContainer: {
+		paddingVertical: 12,
+		margin: 12,
+		paddingBottom: 30,
+		backgroundColor: "#1A232C",
+	},
+	bottomSheetInput: {
+		borderWidth: 1,
+		color: "#fff",
+		paddingHorizontal: 12,
+		borderRadius: 8,
+		paddingVertical: 10,
+		borderColor: "#D9DCDD",
 	},
 })
