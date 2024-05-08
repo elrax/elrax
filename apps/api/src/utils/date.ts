d7a6c9820d7c8bacd93ee2299a46d4f3410469c2
David <enfipy@gmail.com>
2024-05-08 22:50:44 +0800

    feat: small changes to date and button

--- apps/api/src/utils/date.ts
+++ apps/api/src/utils/date.ts
@@ -7,9 +7,5 @@ dayjs.extend(relativeTime)
 
 export const now = () => dayjs.utc()
 export const dateNow = () => now().toDate()
-export const yesterday = () => dayjs.utc().subtract(1, "day")
-export const dateYesterday = () => yesterday().toDate()
-export const tomorrow = () => dayjs.utc().add(1, "day")
-export const dateTomorrow = () => tomorrow().toDate()
 
 export const toRelative = (v: Date) => dayjs.utc(v).fromNow()
